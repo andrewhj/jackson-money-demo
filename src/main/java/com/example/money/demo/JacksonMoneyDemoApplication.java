7145711bf16e3509697b89236caf0d90b1c67f9b
Andrew Johnson <andrew.johnson.h@gmail.com>
2020-09-22 19:00:08 -1000

    Customize RestTemplate
    Build RestTemplate to use wired ObjectMapper.  This ensures the one with java-money Module is added to Template
    
    Signed-off-by: Andrew Johnson <andrew.johnson.h@gmail.com>

--- src/main/java/com/example/money/demo/JacksonMoneyDemoApplication.java
+++ src/main/java/com/example/money/demo/JacksonMoneyDemoApplication.java
@@ -1,8 +1,11 @@
 package com.example.money.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.zalando.jackson.datatype.money.MoneyModule;
 
@@ -19,8 +22,16 @@ public class JacksonMoneyDemoApplication {
     }
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        return new MappingJackson2HttpMessageConverter(objectMapper);
     }
+
+    @Bean
+    public RestTemplate restTemplate(MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter) {
+        return new RestTemplateBuilder()
+            .messageConverters(mappingJackson2HttpMessageConverter)
+            .build();
+    }
+
 }
 
